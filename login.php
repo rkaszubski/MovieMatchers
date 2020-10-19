64f482220ab0eb7dcfd7a8d75bc9b057d7a07ff1
nathanhester-depaul <nhester@depaul.edu>
2020-10-19 10:57:00 -1000

    login now works with new database schema

--- login.php
+++ login.php
@@ -1,23 +1,42 @@
 
 <?php
+	function noSpecialChar($string)
+	{
+		if (preg_match('/[\'^£%&*()}{#~?><>,|=_+¬-]/', $string)) {
+		    // one or more of the 'special characters' found in $string
+				return false;
+		}
+		return true;
+	}
+
 	if(isset($_POST["username"]) && isset($_POST["password"]))
 	{
-		$username = $_POST["username"];
-		$password = $_POST["password"];
-		
-		$pdo = new PDO("sqlite:MMDataBase.db");
-		$stmt = $pdo->query("SELECT * FROM Users WHERE username='$username' AND password='$password'");
-		
-		$row = $stmt ->fetch();
-		if($row)
-		{
-			session_start();
-			$_SESSION["username"] = $row["username"];
-			$_SESSION["role"] = $row["role"];
-			header("Location: movie.php");
-			
+		$username = trim($_POST["username"]);
+		$password = trim($_POST["password"]);
+		if (noSpecialChar($username) && noSpecialChar($password)) {
+			$pdo = new PDO("sqlite:MMDataBase.db");
+			$findUserQuery = "SELECT UID FROM Users WHERE username=?";
+			$stmt = $pdo->prepare($findUserQuery);
+			$stmt->execute([$username]);
+			// returns boolean "false" if user does not exist
+			$uid = $stmt->fetchColumn();
+			if ($uid != false) {
+				$hashPW = md5($password);
+				$findUserPasswordQuery = "SELECT Password FROM Passwords WHERE UserId=?";
+				$stmt2 = $pdo->prepare($findUserPasswordQuery);
+				$stmt2->execute([$uid]);
+				$existingHashedPW = $stmt2->fetchColumn();
+				if ($hashPW == $existingHashedPW) {
+						session_start();
+						$_SESSION["username"] = $username;
+						// How are we getting the role? How should it be set for the session?
+						// $_SESSION["role"] = $row["role"];
+						header("Location: movie.php");
+				}
+			}
 		}
 	}
+		echo "User account does not exist, please try again or register for an account";
 ?>
 
 
@@ -26,14 +45,14 @@
 		<title>Login</title>
 		<link rel="stylesheet" href="stylesheet.css">
 	</head>
-	
+
 	<body>
 		<div class = header style="font-size:4vw;">
-				
+
 				<center>Movie Matchers</center>
 		</div>
 		<div class= container>
-			<div class=overlay>	
+			<div class=overlay>
 				<br>
 				<form id = msform method="POST">
 					<fieldset>
