63e841b2bc80cac9dc0b8aabb0077b209012a7a3
nathanhester-depaul <nhester@depaul.edu>
2020-10-24 22:20:59 -1000

    fixed redundant information in header and footer

--- components/footer.php
+++ components/footer.php
@@ -1,10 +1,3 @@
-<html>
-<head>
-<title></title>
-</head>
-<body>
 <div class="footer" style="background-color:white;">
   <p>Copyright &copy; <?php echo date("Y"); ?> Movie Matcher | Powered by MuskoxV3.0</p>
 </div>
-</body>
-</html>
