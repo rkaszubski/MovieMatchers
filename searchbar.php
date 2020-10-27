429a560d5fc19eec47dc7d94743ea509aafcbd21
alikumail99 <alikumail7860@gmail.com>
2020-10-27 18:59:22 -0500

    Search bar now populates page using Database. Add 'plot' field to Movie table for full functionality

--- searchbar.php
+++ searchbar.php
@@ -43,49 +43,39 @@ function getOmdbRecord($movieName, $ApiKey)
 {
 	//replace spaces " " with plus sign "+"
 	$movieName = str_replace(' ','+',$movieName);
-
 	//Stitch url that gets JSON file
 	$path = "http://www.omdbapi.com/?apikey=$ApiKey&t=$movieName";
-
 	//gets contents of json file
 	$json = file_get_contents($path);
 	return json_decode($json, TRUE);
 }
 
-function populateMovie($string){
-	//if movie does not exist in Database
-	$data = getOmdbRecord("$string", "2f79417c");
-
+//Populate page using Database and not ping OMDB
+function populateMovieFromDB($string){
 	global $title,$director,$actors,$year,$imdbRating,$category,$poster,$rated,$plot;
-	//Uncomment following code to see full Json file from OMDB
-//		echo '<h1> Raw data </h1>';
-//		var_dump($data);
-//		echo '<br><br>';
+	$pdo = new PDO("sqlite:MMDataBase.db");
+	$movieCheckSqlStmt = "SELECT * FROM Movies WHERE Title LIKE :string ";
+	$stmt = $pdo->prepare($movieCheckSqlStmt);
+	$stmt->bindParam(':string',$string);
+	$stmt->execute();
+	$data = $stmt->fetchAll();
 
-//	if movie was not found redirect to search page
-	if(count($data) < 3){
-
-		header('Location: http://localhost:8080/search.php');// replace with hosted URL
-		exit();
-	}
-	else{
-	//Get relevant infomation from returned Jsonfile
-		 $title = $data["Title"];
-		 $director = $data["Director"];
-		 $actors = $data["Actors"];
-		 $year = $data["Year"];
-		 $year = intval($year); //convert to int to add to database
-		 $imdbRating = $data["imdbRating"];
-		 $imdbRating =floatval($imdbRating); //convert to float (cannot do int because it rounds up/down)
-		 $category = $data["Genre"];
-		 $poster = $data["Poster"];
-		 $rated = $data["Rated"];
-		 $plot = $data["Plot"];
-	}
+	$title = $data[0]["Title"];
+	$director = $data[0]["Director"];
+	$actors = $data[0]["Actors"];
+	$year = $data[0]["ReleaseYear"];
+	$year = intval($year); //convert to int to add to database
+	$imdbRating = $data[0]["IMDB_score"];
+	$imdbRating =floatval($imdbRating); //convert to float (cannot do int because it rounds up/down)
+	$category = $data[0]["Category"];
+	$poster = $data[0]["Poster"];
+	$rated = $data[0]["Rated"];
+	$plot = $data[0]["Plot"];
 }
 
-function populateMovieAdd($string){
-	//if movie does not exist in Database
+
+//Populate movies from OMDB
+function populateMovieOMDB($string){
 	$data = getOmdbRecord("$string", "2f79417c");
 	global $pdo;
 	global $title,$director,$actors,$year,$imdbRating,$category,$poster,$rated,$plot;
@@ -96,7 +86,6 @@ function populateMovieAdd($string){
 //
 	//if movie was not found redirect to search page
 	if(count($data) < 3){
-
 		header('Location: http://localhost:8080/search.php');// replace with hosted URL
 		exit();
 	}
@@ -114,17 +103,17 @@ function populateMovieAdd($string){
 		 $rated = $data["Rated"];
 		 $plot = $data["Plot"];
 
-
 		 $newMovieInsertSqlStmt = "INSERT INTO Movies (Title, Director, Actors, ReleaseYear, Poster, IMDB_score, Rated, Category) VALUES (?,?,?,?,?,?,?,?)";
-
 		 $pdo->prepare($newMovieInsertSqlStmt)->execute([$title, $director, $actors, $year, $poster, $imdbRating, $rated, $category]);
 	}
 }
+
+
 if(noSpecialChar($input_term) == true){
 	if(movieExistsInDb($input_term) == true){
-			populateMovie($input_term);
+			populateMovieFromDB($input_term);
 	}else{
-		populateMovieAdd($input_term);
+		populateMovieOMDB($input_term);
 	}
 }else{
 	header('Location: http://localhost:8080/search.php');// replace with hosted URL
